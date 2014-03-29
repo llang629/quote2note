1793105c5f66e8d793e3bbf0da1c211ecc709a46
llang629 <github@larrylang.net>
2014-03-29 12:31:24 -0700

    Updating install

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 # install for AWS Ubuntu 12.04
 # retrieve and run using these commands…
 # wget https://raw.githubusercontent.com/llang629/quote2note/master/install.sh
-# sudo bash install.sh
+# bash install.sh
 
-apt-get update
-apt-get upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
 # directory for application .mid .wav .mp3 files
 echo export Q2N_DIR=./public >>.profile
@@ -15,7 +15,7 @@ echo export rvmsudo_secure_path=0 >>.profile
 
 
 # install sendmail to support Pony.mail
-sudo apt-get install sendmail-bin
+sudo apt-get -y install sendmail-bin
 
 
 # install ruby version manager and ruby
@@ -29,7 +29,7 @@ gem install pony trollop midilib unimidi --no-document #required by application
 # install Phusion Passenger
 # see http://www.modrails.com/documentation/Users%20guide%20Standalone.html#install_on_debian_ubuntu
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 561F9B9CAC40B2F7
-sudo apt-get install apt-transport-https ca-certificates
+sudo apt-get -y install apt-transport-https ca-certificates
 
 echo Phusion Passenger repository configuring for Ubuntu 12.04
 FILE="/etc/apt/sources.list.d/passenger.list"
@@ -49,24 +49,22 @@ EOM
 
 sudo chown root: /etc/apt/sources.list.d/passenger.list
 sudo chmod 600 /etc/apt/sources.list.d/passenger.list
-sudo apt-get update
-sudo apt-get install passenger
+sudo apt-get -y update
+sudo apt-get -y install passenger
 
 
 # see http://wootangent.net/2010/11/converting-midi-to-wav-or-mp3-the-easy-way/
 # install fluidsynth for .mid to .wav
-sudo apt-get install fluidsynth fluid-soundfont-gm
+sudo apt-get -y install fluidsynth fluid-soundfont-gm
 # install lame for .wav to .mp3
-sudo apt-get install lame
+sudo apt-get -y install lame
 
 # install git
-sudo apt-get install git
-git config --global push.default simple
+sudo apt-get -y install git
 
 # install application
-mkdir quote2note
-cd quote2note
 git clone https://github.com/llang629/quote2note.git
+cd quote2note
 
 # start application
 screen rvmsudo passenger start --port 80 --user=ubuntu
