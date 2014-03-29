be44dcd7eaedcc445488acc8c20768a521e23a10
llang629 <github@larrylang.net>
2014-03-29 07:59:43 -0700

    Filepath syntax again

--- routes.rb
+++ routes.rb
@@ -5,17 +5,16 @@ require 'logger'
 $stdout.sync = true
 $stderr.sync = true
 $stderr.puts "q2n: routes.rb starting"
-if ENV.has_key?('Q2N_FOLDER')
-    filepath = ENV['Q2N_FOLDER']+"/"
+
+if ENV.has_key?('Q2N_DIR')
+    filepath = ENV['Q2N_DIR']+"/"
     else
     filepath = ""
 end
 $stderr.puts "q2n: filepath= "+filepath
 
-#FileUtils.mkdir_p('log')
-#log = File.new("log/q2n.log", "a+")
-#$stdout.reopen(log)
-#$stderr.reopen(log)
+@soundfont = "/usr/share/sounds/sf2/FluidR3_GM.sf2"
+$stderr.puts "q2n: soundfount="+@soundfont
 
 # email via /usr/sbin/sendmail
 require 'pony'
@@ -35,19 +34,20 @@ get '/action' do
     $stderr.puts "q2n: route /action with " +@symbol
     
     @midifile = %x[ruby quote2note.rb --symbol #{@symbol}].delete("\n")
-    midifull  = filepath + @midifile
-    @wavfile  = @midifile.sub(/[^.]+\z/,"wav")
-    wavfull   = filepath + @wavfile
-    wav_ret   = system( "fluidsynth -F #{wavfull} /usr/share/sounds/sf2/FluidR3_GM.sf2 #{midifull}" )
-    @mp3file  = @midifile.sub(/[^.]+\z/,"mp3")
-    mp3full   = filepath + @mp3file
-    mp3_ret   = system( "lame #{wavfull} #{mp3full}" )
+    
     #Pony.mail :subject => 'quote2note in use', :body => @midifile
     
     if @midifile.include? "ERROR"
         @symbolinvalid = true
         erb :main
         else
+        @midifull  = filepath + @midifile
+        @wavfile  = @midifile.sub(/[^.]+\z/,"wav")
+        @wavfull   = filepath + @wavfile
+        wav_ret   = system( "fluidsynth -F #{@wavfull} #{@soundfont} #{@midifull}" )
+        @mp3file  = @midifile.sub(/[^.]+\z/,"mp3")
+        @mp3full   = filepath + @mp3file
+        mp3_ret   = system( "lame #{@wavfull} #{@mp3full}" )
         erb :result
     end
 end
