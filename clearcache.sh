1f6a45e73be5d9f619f0d6dac41f99cf95e6deae
Ubuntu <ubuntu@ip-172-31-32-167.(none)>
2014-03-30 01:06:54 +0000

    clearcache edits

--- clearcache.sh
+++ clearcache.sh
@@ -2,10 +2,13 @@
 # install as cron job
 
 # quote2note caches files in public directory
-LOG="/home/ubuntu/quote2note/log/passenger.80.log"
-printf "q2n: clearing public cache of .mid .wav .mp3 files " >>$LOG
+LOG_DIR="/home/ubuntu/quote2note/log"
+sudo chmod go+w $LOG_DIR
+LOG="$LOG_DIR/clearcache.log"
+printf "q2n: clearing public cache of .mid .wav .mp3 files\n" >>$LOG
 date >>$LOG
 ls $Q2N_DIR  >>$LOG
-rm $Q2N_DIR/*.mid >>$LOG
-rm $Q2N_DIR/*.wav >>$LOG
-rm $Q2N_DIR/*.mp3 >>$LOG
+rm -f $Q2N_DIR/*.mid >>$LOG
+rm -f $Q2N_DIR/*.wav >>$LOG
+rm -f $Q2N_DIR/*.mp3 >>$LOG
+sudo chmod go-w $LOG_DIR
