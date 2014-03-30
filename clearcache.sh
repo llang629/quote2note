ecd95cbfc802d1e0315c51b135b2dd890d43242d
llang629 <github@larrylang.net>
2014-03-29 17:45:34 -0700

    Fixing clearcache

--- clearcache.sh
+++ clearcache.sh
@@ -2,7 +2,7 @@
 # install as cron job
 
 # quote2note caches files in public directory
-LOG = /home/ubuntu/quote2note/log/passenger.80.log
+LOG="/home/ubuntu/quote2note/log/passenger.80.log"
 printf "q2n: clearing public cache of .mid .wav .mp3 files " >>$LOG
 date >>$LOG
 ls $Q2N_DIR  >>$LOG
