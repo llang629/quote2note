73feb66e2f6b49c3dee51c6366eea2216aa88cac
llang629 <github@larrylang.net>
2015-03-20 17:49:30 -0700

    Added date output to q2ntest.js

--- q2ntest.sh
+++ q2ntest.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 # repeatedly runs q2ntest.js at random intervals up to MAXSLEEP seconds
+
 MAXSLEEP=300
+
 while true; do
-    node q2ntest.js
+    TZ='America/Los_Angeles' date
+    node q2ntest.js #or nodejs q2ntest.js depending on environment
     sleep $(( $RANDOM % $MAXSLEEP ))
 done
